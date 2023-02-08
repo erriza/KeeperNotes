c90cbb3c207b8a2222c320c30649337962ab0f25
ricsae <eduricsa@hotmail.com>
2023-02-08 13:56:45 -0600

    Updates

---
+++ precache-manifest.b0b9b65bed80b7a273bf1735dc13fde6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bd6cfbb59714f8c33fcb32a9f1db193",
+    "url": "/KeeperNotes/index.html"
+  },
+  {
+    "revision": "630a8566d046e31a4c26",
+    "url": "/KeeperNotes/static/js/2.7bf31d19.chunk.js"
+  },
+  {
+    "revision": "8ffde84b06e05cac812f",
+    "url": "/KeeperNotes/static/js/main.02dcbef8.chunk.js"
+  },
+  {
+    "revision": "449e0623be91a815108c",
+    "url": "/KeeperNotes/static/js/runtime-main.6769fbed.js"
+  }
+]);
